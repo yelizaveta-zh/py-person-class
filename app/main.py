83f20e156c7a1d6b2e82bb35f8cbacad587beb74
Lisa <zhyganiuk.elizaveta@gmail.com>
2024-12-03 12:46:57 +0200

    update recursion

--- app/main.py
+++ app/main.py
@@ -4,9 +4,9 @@ class Person:
     def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
-        self.wife = None
-        self.husband = None
-        Person.people[name] = self
+        self.people.update({
+            name: self
+        })
 
 
 def create_person_list(people: list) -> list:
